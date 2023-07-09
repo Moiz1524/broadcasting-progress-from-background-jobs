572e010cbd2ecf79d6a6cfa0a5ffbf0b1e366dda
Moiz Ali <amoiz4142@gmail.com>
2023-07-10 00:30:13 +0500

    Init commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "heavy_tasks#index"
+  resources :heavy_tasks, only: [:index, :create]
+end
